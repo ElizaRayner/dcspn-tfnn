5e0f74eef62b0aad3d4307c9ad4959aef16c88fb
Elizabeth Rayner <elizarayner@sasktel.net>
2019-08-23 14:15:09 -0600

    Update FindSizeAndUseFunction.py

--- FindSizeAndUseFunction.py
+++ FindSizeAndUseFunction.py
@@ -1,13 +1,14 @@
 """
 Creates a DCSPN to find the size of the DCSPN weights. Then uses the function to build the NN and attach the weights to a new DCSPN
+Runs using libraries from https://github.com/jhonatanoliveira/dcspns
 """
 
 from __future__ import print_function
 
 from tensorflow.keras import layers #actually imports the keras
 from dcspn.spn import SumProductNetwork
-from dcspn.layers import SumLayer, ProductLayer, GaussianLeafLayer
-from dcspn.utilities import Database, plot_cost_graph
+from dcspn.layers import SumLayer, ProductLayer, GaussianLeafLayer, LeafLayer
+from dcspn.utilities import Database, plot_cost_graph, kahn_topsort
 
 
 import tensorflow as tf
@@ -42,6 +43,8 @@ def create_nn(spn_struct, nn_struct):
     previous = X
 
 
+    nn_layers = []  #For keeping track of each layer
+
     counter = 0 #Counter is for naming tensors
     
     #for each layer in the nn
@@ -64,10 +67,11 @@ def create_nn(spn_struct, nn_struct):
         #Creating the layer
         previous= tf.nn.relu(tf.matmul(W1, previous) + b1)
         
+        nn_layers.append(previous)  #Append the new layer
 
         counter = counter + 1 #Add to the counter for naming
         
-    print("Out of loop")
+    
 
     # Weight for the final layer
     W1 = tf.get_variable(
@@ -89,11 +93,142 @@ def create_nn(spn_struct, nn_struct):
     final_output= tf.nn.relu(tf.matmul(W1, previous) + b1)
 
 
+    #Append to keep track of the final layer
+    nn_layers.append(final_output)
+
+
+
     #return the final output
     return final_output
    
 
 
+
+
+
+def get_spn_layer_sizes(spn):
+
+    spn_struct = [] #for storing the shape of the DCSPN
+
+    print(spn.layers_graph["children"])
+    #print(spn.layers_graph["children"][0])
+    #previous_layer = spn.layers_graph["children"][0]
+
+
+    
+
+    #variables necessary to calculate size of layers
+    forward_input = spn.inputs
+
+    forward = {
+        forward_input: {
+            "output_shape": spn.input_shape,
+            "output": forward_input
+        }
+    }
+
+    #For each layer, most of this code is modified from the build_forward function
+    for curr_layer in spn.layers_graph["children"]:
+
+        children_output = []
+        children_output_shape = []
+            
+
+        #If the current layer is a leaf layer
+        if isinstance(curr_layer, LeafLayer):
+            children_output_shape = spn.input_shape
+            children_output = forward_input
+
+
+
+
+
+
+
+        if isinstance(curr_layer, ProductLayer):
+            children_out_width = 0
+            for child in spn.layers_graph["children"][curr_layer]:
+                # OBS: current implementation only allows
+                # sideways connectivity so the height of all
+                # children should be the same.
+                child_shape = forward[
+                    child]["output_shape"]
+                if len(children_output_shape) > 0 and(
+                        (children_output_shape[0] != child_shape[0]) or
+                        (children_output_shape[2] != child_shape[0])):
+                    raise ValueError("Children of product layer should\
+                        have only different width.")
+                # Add width
+                children_output_shape = child_shape
+                children_out_width += child_shape[1]
+                children_output.append(forward[
+                    child]["output"])
+            # Concatenate children outputs in the width channel
+            # (sideways) for decomposability maintenance.
+            children_output = tf.concat(children_output, axis=2) \
+                if len(children_output) > 1 else children_output[0]
+            # Only the width axis is altered
+            children_output_shape[1] = children_out_width
+
+            print("children_output_shape: ", children_output_shape)
+
+
+            
+
+
+
+
+        if isinstance(curr_layer, SumLayer):
+            children_out_channels = 0
+            for cidx, child in enumerate(
+                    spn.layers_graph["children"][curr_layer]):
+                # Add channels
+                child_ch_amt = forward[child]["output_shape"][2]
+                children_output_shape = forward[
+                    child]["output_shape"]
+                children_out_channels += child_ch_amt
+                children_output.append(forward[
+                    child]["output"])
+            # Concatenate children outputs in the
+            # out channel axis (from behind) for
+            # satisfying completeness
+            children_output = tf.concat(children_output, axis=3) \
+                if len(children_output) > 1 else children_output[0]
+            # Only channel (depth) is altered when joining children
+            # layers in a SumLayer
+            children_output_shape[2] = children_out_channels
+
+
+            #children_output_shape[3] = curr_layer.out_channels
+            print("children_output_shape: ", children_output_shape)
+
+
+
+            #Append the weight sizes to the array
+            children_output_shape = [children_output_shape[0], children_output_shape[1],
+                          children_output_shape[2], curr_layer.out_channels]
+            print("_var_shape: ", children_output_shape)
+            spn_struct.append(children_output_shape)   #append it to the array
+            print(spn_struct)
+
+
+
+
+
+
+        #Prepare values for the next loop
+        curr_layer_output = children_output_shape
+        curr_layer_output_shape = curr_layer.compute_output_shape(
+            children_output_shape)
+        # Save current layer in forward computational graph
+        forward[curr_layer] = {}
+        forward[
+            curr_layer]["output_shape"] = curr_layer_output_shape
+        forward[
+            curr_layer]["output"] = curr_layer_output
+        
+
+    return spn_struct   #return the sizes of the weights
 
 
 
@@ -200,45 +335,22 @@ def define_SPN():
 
 
 
-    #TODO: Landmark-2
-    #defining loss function and optimizer, calls build forward to build tensorflow graph
-    #Before this point the layers were established but have no values
-    forward = spn_model.compile(learning_rate=0.01,
-                        optimizer="adam",
-                        reuse=False,
-                        external_weights=None) #This will be the tensor that is sliced and reshaped
-
-
-
-
     print("################################")
+    print(spn_model)
 
 
 
 
-    #TODO: Landmar-1
-    spn_struct = [] #for storing the shape of the DCSPN
-    #for each layer
-    for layer in spn_model.layers:
-        #Test if the layer is a sum layer
-        if isinstance(layer, SumLayer):
-            
-            hold_shape = layer.weights.get_shape().as_list()    #get the shape of the weight as a list
-            spn_struct.append(hold_shape)   #append it to the array
 
-
-    print("Struct:")
-    print(spn_struct)
-
-    return spn_struct   #return the sizes of the weights
+    spn_model.leaf_layer.means = leaf_means
+    spn_model.leaf_layer.stds = leaf_stds
 
 
 
-# TODO: create new function called get_spn_layer_sizes(spn)
-# This function contains code from Landmark-1
-# Remove code from Landmark-2, that is no "compile" or "build_forward"
-# Therefore, we need to traverse the DCSPN graph using "lasyers_graph" with either "children" or "parents"
-# For each SumLayer, compute the weights shape using the previous (parent) layer size and the "out_channel" size of this current SumLayer. Take a look at "SumLayer.build" and "calling build in build_forward".
+    spn_struct = get_spn_layer_sizes(spn_model)
+    return spn_struct, spn_model
+
+
 
 
 
@@ -401,7 +513,7 @@ print(leaf_stds.shape)
 
 
 #get the struct of the DCSPN
-spn_struct = define_SPN()
+spn_struct, spn = define_SPN()
 
 
 
@@ -420,104 +532,6 @@ external_tensor = create_nn(spn_struct, nn_struct)
 print(external_tensor)
 
 
-#TODO: No new SPN, use the previous one built in "build_spn"
-
-
-#From here down is the actually trainable DCSPN that loads the weights
-initializer = "uniform" #for the layer parameters
-
-#The share parameter is related to the weights. 
-#If share_parameter is false than the weights tensor will be the same size as the sum layer tensor
-#If share_parameter is true that the weights tensor is 1x1xc where c is then number of leaf components. 
-#In this case the weights are the same for all input pixels
-share_parameters = False
-
-#Determines if the sum node will perform of max or sum operation
-#if false it will perform a sum, if true if will take the maximum child
-hard_inference = False
-
-#saves the shape of the input
-input_shape = [img_height, img_width, img_channel]
-
-
-#create an spn, this will hold all of the layers and be used for training and inferences
-spn = SumProductNetwork(input_shape=input_shape, reuseValue=reuse)
-
-# Build leaf layer
-leaf_layer = GaussianLeafLayer(num_leaf_components=leaf_components)#these leaves will have the Gaussian distribution
-#Each leaf, there will be 4/pixel as there are 4 leaf_components, will be a Gaussian distribution using the means and stds previously calculated
-
-
-spn.add_layer(leaf_layer)#adds the leaf layer to the graph
-spn.set_leaf_layer(leaf_layer)#sets it as the leaf layer
-
-
-
-#create a sum layer
-#A sum layer, for each pixel, combines the channels of that pixel by either adding all the values togethher or choosing the maximum value
-#the sum layer will shrink the number of channels but not change the width or height
-#if oc is the number of out channels and c is the number of original channels then the sum layer changed from wxhxc to wxhxoc
-sum_layer = SumLayer(out_channels=10,  #out_channels chooses how many output channnels there will be
-                    hard_inference=hard_inference,
-                    share_parameters=share_parameters,
-                    initializer=initializer)
-#because this is a toy example there is only one out channel as we only want one sum layer. The hard_inference and share_parameters are described above
-#One out channel means that the tensor will be reduced from wxhxc to wxhx1
-
-
-#connect the sum layer to the tree, the sum layer is the parent of the leaf layer
-spn.add_forward_layer_edge(leaf_layer, sum_layer)
-#this builds a tree upwards, so leaf_layer is the child of the sum_layer, building up towards the root
-
-
-
-
-#the pool window is used by the product layer
-#the size of the pooling window setermines which pixels will be filtered together
-#The size of the pooling window determines the width and height of the new tensor that is produced
-#if pwxph is the size of the pooling window and the input tensor is wxhxc then the new tensor is (w/pw)x(h/ph)xh
-pool_window = (img_width,img_height)
-#Because this is a toy example and only one product layer is desired the size of the pooling window should be thhe same as the size of the input
-
-sum_pooling = True #to create and average sum before pooling
-
-
-#create a product layer
-#A product layer is like a filter layer in a CNN. It reduced the height and width based off the size of the pooling window
-#The number of channels will remain the same
-product_layer = ProductLayer(pooling_size=pool_window,
-                              sum_pooling=sum_pooling)
-#add the product layer to the graph. It will be the parent of the sum layer
-spn.add_forward_layer_edge(sum_layer, product_layer)
-
-
-
-
-
-root_layer = SumLayer(out_channels=1,  #out_channels chooses how many output channnels there will be
-                    hard_inference=hard_inference,
-                    share_parameters=share_parameters,
-                    initializer=initializer)
-
-spn.add_forward_layer_edge(product_layer, root_layer)
-
-
-
-#The product layer is now 1x1x1, meaning it can no longer be reduced
-#This means that it is the root layer an needs to be assigned as such
-#You can create a special layer just to be the root if you wish but it is not necessary
-spn.set_root_layer(root_layer)
-print("The model is now created. Its name is spn.")
-
-
-#This model has 3 layers: the leaf layer, a sum layer  and a product layer
-#The leaf layer contains the values from the input and is a tensor of size wxhxc where w and h are the dimensions of the input and c is the number of leaf_components
-#The next layer is a sum layer which sums together the channels. Because this is a toy example the sum layer here only has one out channel in order to shrink the model
-#In the sum layer the model goes from wxhxc to wxhx1 with only one channel as the output 
-#The final layer is the product layer. Once again, because this is a toy example only one product layer is desired. 
-#As such the pooling window is the same size as the input(wxh) so the tensor goes from size wxhx1 to 1x1x1
-#Because the size is 1x1x1 this layer cannot be reduced therefore this layer is also the root
-#It must be set as the root so that the spn is aware of it
 
 
 print("Starting to fit the model")
@@ -560,7 +574,7 @@ feed_means_stds = {
 #This loop will repeat within the function for the specified number of epochs
 #The costs returned is the error over time, in other words, how different the produced images are from the correct image
 #The cost is Negative Log-Likelihood(NLL)
-spn.fit(train_data=train_imgs, epochs=0, add_to_feed=feed_means_stds, minibatch_size=64)
+spn.fit(train_data=train_imgs, epochs=2, add_to_feed=feed_means_stds, minibatch_size=64)
 
 
 
@@ -725,7 +739,7 @@ log_mse.close()
 
 
 
-"""
+
 #This just prints some information about the DCSPN
 print("\n\n\nNOW THE DIMENSIONS")
 print("layer 0- leaf layer")
@@ -747,7 +761,7 @@ print("\nlayer 3- root layer")
 print(spn.layers[3])
 #print(spn.layers[3].shape)
 print(spn.layers[3].weights)
-"""
+
 
 
 
